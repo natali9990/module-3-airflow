640a985f6349eeff155f56bb4f51eed6b85ee634
natali9990 <75131135+natali9990@users.noreply.github.com>
2021-02-25 15:47:35 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -2,34 +2,34 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
+def task_rocket(i,j):
+    default_args = {
+        "owner": "airflow",
+        "depends_on_past": False,
+        "start_date": datetime(2005, 1, 1),
+        "email": ["airflow@airflow.com"],
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retries": 0,
+        "retry_delay": timedelta(minutes=5),
+    }
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime(2005, 1, 1),
-    "email": ["airflow@airflow.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 0,
-    "retry_delay": timedelta(minutes=5),
-}
+    dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
-
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r falcon1", 
-    dag=dag
-)
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{params.rocket}}/data.csv", 
-    params={"rocket": "falcon1"}, # falcon1/falcon9/falconheavy
-    dag=dag
-)
-t1 >> t2
-#task("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data",{"rocket": "all"})
-#task("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r falcon1",{"rocket": "falcon1"})
+    t1 = BashOperator(
+        task_id="get_data", 
+        bash_command=i, 
+        dag=dag
+    )
+    t2 = BashOperator(
+        task_id="print_data", 
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{params.rocket}}/data.csv", 
+        params=j, # falcon1/falcon9/falconheavy
+        dag=dag
+    )
+    t1 >> t2
+task_rocket("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data",{"rocket": "all"})
+task_rocket("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r falcon9",{"rocket": "falcon9"})
 #rocket_lst=["falcon1","falcon9","falconheavy"]
 #for i in rocket_lst:
     #task("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r {{params.rocket}}",{"rocket": i})
