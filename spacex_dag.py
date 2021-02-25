0172462fd21cbea964ab4af423a286ebb4fd2f2b
natali9990 <75131135+natali9990@users.noreply.github.com>
2021-02-25 15:57:53 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -30,7 +30,7 @@ def task_rocket(i,j):
     )
     t1 >> t2
 task_rocket("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data",{"rocket": "all"})
-task_rocket("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r falcon9",{"rocket": "falcon9"})
+#task_rocket("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r falcon9",{"rocket": "falcon9"})
 #rocket_lst=["falcon1","falcon9","falconheavy"]
 #for i in rocket_lst:
     #task("python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data -r {{params.rocket}}",{"rocket": i})
