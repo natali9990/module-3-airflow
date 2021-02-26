881823049e67d0b1d809bbbea11a4dd315e2adcb
natali9990 <75131135+natali9990@users.noreply.github.com>
2021-02-26 08:01:20 +0300

    Update git_sync.py

--- git_sync.py
+++ git_sync.py
@@ -8,7 +8,7 @@ from airflow.utils.dates import days_ago
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2021, 2, 25, 6, 47, 0),
+    'start_date': datetime(2021, 2, 26, 5, 2, 0),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
