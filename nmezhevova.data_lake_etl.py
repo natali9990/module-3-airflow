66b562e9310f79ccb0ec3a9e0119321fbf9cb6c0
natali9990 <75131135+natali9990@users.noreply.github.com>
2021-04-09 21:20:26 +0300

    Update nmezhevova.data_lake_etl.py

--- nmezhevova.data_lake_etl.py
+++ nmezhevova.data_lake_etl.py
@@ -8,7 +8,7 @@ USERNAME = 'nmezhevova'
 
 default_args = {
     'owner': USERNAME,
-    'start_date': datetime(2012, 1, 1, 0, 0, 0)
+    'start_date': datetime(2013, 1, 1, 0, 0, 0)
 }
 
 dag = DAG(
@@ -17,16 +17,36 @@ dag = DAG(
     description='Data Lake ETL tasks',
     schedule_interval="0 0 1 1 *",
 )
+insert_dict={'billing':["user_id, billing_period, service, tariff, cast(sum as INT), cast(created_at as DATE)",'created_at'],
+             'issue':["cast(user_id as INT), cast(start_time as TIMESTAMP), cast(end_time as TIMESTAMP), title, description, service",'start_time'],
+             'payment':["user_id, pay_doc_type, pay_doc_num, account,phone,billing_period,cast(pay_date as DATE),cast(sum as INT)",'pay_date'],
+             'traffic':["user_id,cast(`timestamp` as TIMESTAMP),device_id, device_ip_addr, bytes_sent,bytes_received",'cast(`timestamp` as TIMESTAMP)'],
+             'dm_user_traffic':["user_id, max(bytes_received),min(bytes_received),avg(bytes_received)",'event']}
 
-ods_billing = DataProcHiveOperator(
-    task_id='ods_billing',
-    dag=dag,
-    query="""
-        insert overwrite table nmezhevova.ods_billing partition (year='{{ execution_date.year }}') 
-        select user_id, billing_period, service, tariff, cast(sum as INT), cast(created_at as DATE) from nmezhevova.stg_billing where year(created_at) = {{ execution_date.year }};
-    """,
-    cluster_name='cluster-dataproc',
-    job_name=USERNAME + '_ods_billing_{{ execution_date.year }}_{{ params.job_suffix }}',
-    params={"job_suffix": randint(0, 100000)},
-    region='europe-west3',
-)
+for i,j in insert_dict.items():
+    if i!='dm_user_traffic':
+        ods_table = DataProcHiveOperator(
+            task_id=f'ods_{i}',
+            dag=dag,
+            query=f"""
+            insert overwrite table nmezhevova.ods_{i} partition (year='{{ execution_date.year }}') 
+            select {j[0]} from nmezhevova.stg_{i} where year({j[1]}) = {{ execution_date.year }};
+            """,            
+            cluster_name='cluster-dataproc',
+            job_name=USERNAME + f'_ods_{i}_{{ execution_date.year }}_{{ params.job_suffix }}',
+            params={"job_suffix": randint(0, 100000)},
+            region='europe-west3',
+        )
+    else:
+        dm_table = DataProcHiveOperator(
+            task_id=i,
+            dag=dag,
+            query=f"""
+            insert overwrite table nmezhevova.{i} partition (year='{{ execution_date.year }}') 
+            select {j[0]} from nmezhevova.ods_traffic where year({j[1]}) = {{ execution_date.year }} GROUP BY user_id;
+            """,            
+            cluster_name='cluster-dataproc',
+            job_name=USERNAME + f'_{i}_{{ execution_date.year }}_{{ params.job_suffix }}',
+            params={"job_suffix": randint(0, 100000)},
+            region='europe-west3',
+        )
