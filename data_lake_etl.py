a65a57a65cd5470a66edfe32d30ad658668849ea
natali9990 <75131135+natali9990@users.noreply.github.com>
2021-04-09 17:47:59 +0300

    Create data_lake_etl.py

---
+++ data_lake_etl.py
@@ -0,0 +1,32 @@
+from datetime import timedelta, datetime
+from random import randint
+
+from airflow import DAG
+from airflow.contrib.operators.dataproc_operator import DataProcHiveOperator
+
+USERNAME = 'emateshuk'
+
+default_args = {
+    'owner': USERNAME,
+    'start_date': datetime(2012, 1, 1, 0, 0, 0)
+}
+
+dag = DAG(
+    USERNAME + '_data_lake_etl',
+    default_args=default_args,
+    description='Data Lake ETL tasks',
+    schedule_interval="0 0 1 1 *",
+)
+
+ods_billing = DataProcHiveOperator(
+    task_id='ods_billing',
+    dag=dag,
+    query="""
+        insert overwrite table emateshuk.ods_billing partition (year='{{ execution_date.year }}') 
+        select * from emateshuk.stg_billing where year(created_at) = {{ execution_date.year }};
+    """,
+    cluster_name='cluster-dataproc',
+    job_name=USERNAME + '_ods_billing_{{ execution_date.year }}_{{ params.job_suffix }}',
+    params={"job_suffix": randint(0, 100000)},
+    region='europe-west3',
+)
